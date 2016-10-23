2f6df01b35ab6f625475f5b417b4ca4184170ea7
abwhitley <a.blakewhitley@gmail.com>
2016-10-23 18:25:37 -0400

    Added enum for Ingredients, Working toward an ingredients table view

---
+++ RecipeBook/IngredientCell.swift
@@ -0,0 +1,24 @@
+//
+//  IngredientCell.swift
+//  RecipeBook
+//
+//  Created by Austins Work on 10/23/16.
+//  Copyright © 2016 AustinsIronYard. All rights reserved.
+//
+
+import UIKit
+
+class IngredientCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ RecipeBook/IngredientTableView.swift
@@ -0,0 +1,14 @@
+//
+//  IngredientTableViewCell.swift
+//  RecipeBook
+//
+//  Created by Austins Work on 10/23/16.
+//  Copyright © 2016 AustinsIronYard. All rights reserved.
+//
+
+import UIKit
+
+class IngredientTableView: UITableView {
+    var ingredients = Ingredients(ingredientList: [])
+    
+}
